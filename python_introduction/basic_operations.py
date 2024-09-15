6f4d154778e75638e3a24550c162705b0504e79d
Maryem FATIMI <maryemfatimi@gmail.com>
2024-09-15 13:12:36 +0100

    adding the basic_operations.py that conatin the addition soustraction and the multiplication for the two numbers 10 and 5

---
+++ python_introduction/basic_operations.py
@@ -0,0 +1,9 @@
+#!/bin/bash
+number1 = 10
+number2 = 5
+Addition = number1 + number2
+print("Addition of " +  str(number1) + " and "+ str(number2) + " is " + str(Addition))
+Subtraction = number1 - number2
+print("Subtraction of " + str(number1) + " and " + str(number2) + " is " + str(Subtraction))
+Multiplication = number1 * number2
+print("Multiplication of " + str(number1) + " and " + str(number2) + " is " + str(Multiplication))
