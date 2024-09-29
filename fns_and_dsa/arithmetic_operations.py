bcb9dfd5364d32f91c3e8d582e91b0f37cdb54ca
Maryem FATIMI <maryemfatimi@gmail.com>
2024-09-29 21:48:16 +0100

    adding arithmetic function based on the user input

--- fns_and_dsa/arithmetic_operations.py
+++ fns_and_dsa/arithmetic_operations.py
@@ -1,20 +1,17 @@
 def perform_operation(num1, num2, operation):
-    match operation:
-        case 'add':
-            return num1 + num2
-        case 'subtract':
-            return num1 - num2
-        case 'multiply':
-            return num1 * num2
-        case 'divide':
-            if (num2 == 0):
-                print("you can't apply division by zero")
-                return 0
-            else:
-                return num1 / num2
-        case _:
-                print("Invalid operation; shoose one of these (add, subtract, multiply, divide)")
-                return 0
+    if operation == 'add':
+        return num1 + num2
+    elif operation == 'subtract':
+        return num1 - num2
+    elif operation == 'multiply':
+        return num1 * num2
+    elif operation == 'divide':
+        if num2 == 0:
+            return "Error: Division by zero"
+        return num1 / num2
+    else:
+        return("Invalid operation; shoose one of these (add, subtract, multiply, divide)")
+                 
 
 
     
