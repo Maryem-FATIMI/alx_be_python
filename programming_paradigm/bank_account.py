dc4da5068ac4cf43ae1cdf9cad34717bd53e0605
Maryem FATIMI <maryemfatimi@gmail.com>
2024-10-06 15:22:25 +0100

    implementing a BankAccount class that encapsulates banking operations

---
+++ programming_paradigm/bank_account.py
@@ -0,0 +1,19 @@
+class BankAccount:
+    
+    def __init__(self, account_balance):
+        self.user_account_balance = account_balance
+
+    def deposit(self, amount):
+        self.user_account_balance =+ amount
+
+    def withdraw(self, amount):
+        amount = float(amount)
+        if self.user_account_balance > amount:
+            self.user_account_balance =- amount
+            return True
+        else:
+            False
+    
+    def display_balance(self):
+         self.user_account_balance = float(self.user_account_balance)
+         print(f"Current Balance: ${self.user_account_balance:.2f}")
