a6565e177ad3325684118eeee77f73f96734a5ec
Maryem FATIMI <maryemfatimi@gmail.com>
2024-10-13 20:20:22 +0100

    commiting the class_static_methods_demo.py file

--- oop/main.py
+++ oop/main.py
@@ -53,3 +53,17 @@ def main2():
 
 if __name__ == "__main__":
     main2()
+
+    from class_static_methods_demo import Calculator
+
+def main3():
+    # Using the static method
+    sum_result = Calculator.add(10, 5)
+    print(f"The sum is: {sum_result}")
+
+    # Using the class method
+    product_result = Calculator.multiply(10, 5)
+    print(f"The product is: {product_result}")
+
+if __name__ == "__main__":
+    main3()
