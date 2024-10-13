a5c50a397bdc4a77340e5b24beed118abe5d2531
Maryem FATIMI <maryemfatimi@gmail.com>
2024-10-13 17:42:06 +0100

    commiting the boo class

--- oop/book_class.py
+++ oop/book_class.py
@@ -11,4 +11,4 @@ class Book:
         return print(f"{self.title} by {self.author}, published in {self.year}")
     
     def __repr__(self):
-        return f"Book('{self.title}', '{self.author}', {self.year})"
+        return f"{self.title}', '{self.author}', {self.year}"
